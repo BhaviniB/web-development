60720c894e6c2dbfb798b7b4f9397f45c062ca79
BhaviniB <lassiebhavini@gmail.com>
2018-02-20 15:38:03 +0530

    prime

--- COMPLETE WEB DEVELOPER/ff.php
+++ COMPLETE WEB DEVELOPER/ff.php
@@ -14,8 +14,8 @@ $myBool=true;
 echo $myBool;
 
 //variable within variable
-$dis="name"
-echo $$dis; //will display BHAVINI
+$dis="$name";
+echo $dis; //will display BHAVINI
 
 $myarray[]= array("bhavini","manu","monika");
 $myarray[]="ashwin";
@@ -24,7 +24,7 @@ print_r($myarray);
 echo "<br><br>";
 $anotherarray[0]="pizza";
 $anotherarray[1]="burger";
-$anotherarray[0]="yoghurt";
+$anotherarray[2]="yoghurt";
 $anotherarray["my fav food"]="..";
 
 print_r($anotherarray);
@@ -41,15 +41,10 @@ unset($yetanotherarray["France"]);
 //size
 sizeof($yetanotherarray);
 $fam[]=array("manu","bhavini","monika");
-foreach($fam $key==> $value)
+foreach($fam $key=> $value)
 { $fam[$key]=$value."batra";
 echo("$key"."has a value"."$value");	
 }
-
-
-
-
-
 
 
 
---
+++ COMPLETE WEB DEVELOPER/new.php
@@ -0,0 +1,12 @@
+<?php
+print_r($-_GET);
+echo "hi there ".$_GET['name']."!";
+?>
+
+
+
+<p> What is your name?</p>
+<form>
+ <input name="name" type="text">
+ <input type="submit" value="go">
+ </form>

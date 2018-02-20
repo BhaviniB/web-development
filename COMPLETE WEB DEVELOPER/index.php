3b3a609f227311f1d52ab082f480adc67a9ab58f
BhaviniB <lassiebhavini@gmail.com>
2018-02-20 06:01:03 +0530

    array basics

---
+++ COMPLETE WEB DEVELOPER/index.php
@@ -0,0 +1,51 @@
+<?php
+
+
+
+echo "hello";
+$name="BHAVINI";
+echo $name;
+echo "$name";
+$s1="<p>hey</p>"; 
+
+$s2="<p>you suck</p>";
+echo $s1." ".$s2;
+$myBool=true; 
+echo $myBool;
+
+//variable within variable
+$dis="name"
+echo $$dis; //will display BHAVINI
+
+$myarray[]= array("bhavini","manu","monika");
+$myarray[]="ashwin";
+echo ($myarray) ; //doesnt do anything but tell is an array
+print_r($myarray);
+echo "<br><br>";
+$anotherarray[0]="pizza";
+$anotherarray[1]="burger";
+$anotherarray[0]="yoghurt";
+$anotherarray["my fav food"]="..";
+
+print_r($anotherarray);
+
+//associative array
+$yetanotherarray=array(
+"France"=>"French",
+"USA"=>"English",
+"Germany"=>"German");
+
+print_r($yetanotherarray);
+//will delete from the array
+unset($yetanotherarray["France"]);
+//size
+sizeof($yetanotherarray);
+
+
+
+
+
+
+
+
+?>

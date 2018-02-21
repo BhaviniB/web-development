8f17c0091698391c401d8ab06684c5b2cce843a3
BhaviniB <lassiebhavini@gmail.com>
2018-02-21 23:46:15 +0530

    included documentation

--- COMPLETE WEB DEVELOPER/contactform.php
+++ COMPLETE WEB DEVELOPER/contactform.php
@@ -19,7 +19,7 @@ FILTER_VALIDATE_EMAIL)===false)
 }
 if($error!="")
 {
-	$erroe=<'div class="alert alert-danger" role="alert">
+	$error=<'div class="alert alert-danger" role="alert">
   There were errors!'.error.
 '</div>'
 	
@@ -96,7 +96,7 @@ $error=<'div class="alert alert-danger" role="alert">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   
   <script type="text/javascript">
-  
+//error checking  
   $("form).submit(function(e)
   {
 	 
